cc35f188bb2cc5dd4060ff98de126ea50b050fcd
brutekoon <brutekoon@gmail.com>
2020-04-08 02:19:33 +0900

    remark

--- hook_libbinder.js
+++ hook_libbinder.js
@@ -2,6 +2,8 @@
 
 
 Java.perform(function(){
+	
+	// ioctl 시스템콜을 후킹 포인트로 선정, 커널과 가장 가까운 단계에서 바인더 메시지를 보내는데 사용되는 시스템콜
 	var ioctl = Moudle.findExportByName("libbinder.so", "ioctl");
 	Interceptor.attach(ioctl, {
 		console.log("ioctl hook");
