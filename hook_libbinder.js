bfc7b2169ea821c485bb49b7d2eec68f77739ea9
brutekoon <brutekoon@gmail.com>
2020-04-13 00:16:03 +0900

    args init

--- hook_libbinder.js
+++ hook_libbinder.js
@@ -7,6 +7,15 @@ Java.perform(function(){
 	var ioctl = Moudle.findExportByName("libbinder.so", "ioctl");
 	Interceptor.attach(ioctl, {
 		console.log("ioctl hook");
+		
+		onEnter: function(args){
+			// args[0] :파일 디스크립터
+			// args[1] : 명령을 나타내는 정수
+			// args[2] : 특정 구조체를 가리키는 포인터
+			var fd = args[0];
+			var cmd = args[1];
+			console.log(args);
+		}
 	})
 
 
